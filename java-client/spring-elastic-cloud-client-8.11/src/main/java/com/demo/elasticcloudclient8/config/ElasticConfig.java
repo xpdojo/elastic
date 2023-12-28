ba867ab14c67eb55c5745065555b69da09af3c9b
Changsu Im <imcxsu@gmail.com>
2023-12-28 11:22:06 +0900

    RestHighLevelClient 설명 추가

--- java-client/spring-elastic-cloud-client-8.11/src/main/java/com/demo/elasticcloudclient8/config/ElasticConfig.java
+++ java-client/spring-elastic-cloud-client-8.11/src/main/java/com/demo/elasticcloudclient8/config/ElasticConfig.java
@@ -4,9 +4,13 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientOptions;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +28,31 @@ public class ElasticConfig extends ElasticsearchConfigurationSupport {
     private Integer port;
     @Value("${elasticsearch.api-key}")
     private String apiKey;
+
+    /**
+     * Spring Data Elasticsearch 5.x부터 {@link RestHighLevelClient}가 deprecated 되고
+     * {@link ElasticsearchClient}를 사용한다. {@code AbstractElasticsearchConfiguration} 추상 클래스를 상속받으면
+     * 반환 타입이 {@link RestHighLevelClient}이기 때문에 상위 클래스
+     * {@link ElasticsearchConfigurationSupport}를 상속한다.
+     *
+     * <pre>
+     * // ElasticsearchConfigurationSupport에는 elasticsearchClient()가 없다.
+     * // AbstractElasticsearchConfiguration extends ElasticsearchConfigurationSupport
+     * <code>@Bean</code>
+     * public abstract RestHighLevelClient elasticsearchClient();
+     * </pre>
+     *
+     * @see <a href="https://docs.spring.io/spring-data/elasticsearch/docs/5.1.6/reference/html/#elasticsearch-migration-guide-4.4-5.0.new-clients">Upgrading from 4.4.x to 5.0.x</a>
+     */
+    // @Bean
+    public RestHighLevelClient deprecated() {
+        final Header[] headers = new Header[]{new BasicHeader("Authorization", "ApiKey " + apiKey)};
+        final RestClientBuilder builder =
+                RestClient
+                        .builder("cloud-id")
+                        .setDefaultHeaders(headers);
+        return new RestHighLevelClient(builder);
+    }
 
     /**
      * low-level client
